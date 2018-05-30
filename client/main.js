7df04a9e71f2eceddb525e18c43cc8ef59986b4f
Fred Ma <fma1@terpmail.umd.edu>
2018-05-30 16:45:44 -0400

    Added main component

---
+++ client/main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Navbar } from './components'
+import Content from './content'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Content />
+    </div>
+  )
+}
+
+export default App
