7598c4fa939fe90ebe3f5dddc9e55c6424cf3214
Fred Ma <fma1@terpmail.umd.edu>
2018-06-01 16:33:11 -0400

    Added NavBar

---
+++ client/components/navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const NavBar = () =>
+  <nav>
+    <ul className="navbar">
+      <li>About Me</li>
+      <li>Projects</li>
+      <li>Contact Me</li>
+    <ul>
+  </nav>
+
+export default About
