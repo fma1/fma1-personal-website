b79d903a958c74f26b628307b761890eb074b3d5
Fred Ma <fma1@terpmail.umd.edu>
2018-06-03 20:44:06 -0400

    Added active class

--- client/navbar.js
+++ client/navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 const Navbar = () =>
   <nav>
     <ul id="navbar">
-      <li>About Me</li>
+      <li className="active">About Me</li>
       <li>Projects</li>
       <li>Contact Me</li>
     </ul>
