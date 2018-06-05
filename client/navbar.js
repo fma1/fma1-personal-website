23e76bf8c6a12be2a9066083bace0905c44ea7bf
Fred Ma <fma1@terpmail.umd.edu>
2018-06-04 20:14:40 -0400

    Added state to navbar

--- client/navbar.js
+++ client/navbar.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import history from './history'
 
-const Navbar = () =>
-  <nav>
-    <ul id="navbar">
-      <li className="active">About Me</li>
-      <li>Projects</li>
-      <li>Contact Me</li>
-    </ul>
-  </nav>
+export default class Navbar extends Component {
+  constructor() {
+    this.state = { selectedId: 'about' }
+  }
 
-export default Navbar 
+  handleChange(e) {
+    const selectedId = e.target.getAttribute('id')
+    e.preventDefault()
+    this.setState({ selectedId })
+    history.push(`/${selectedId}`)
+  }
+
+  render() {
+    <nav>
+      <ul id="navbar">
+        <li id="about" className={selectedId === 'about' ? 'active' : ''}>About Me</li>
+        <li id="projects" className={selectedId === 'projects' ? 'active' : ''}>Projects</li>
+        <li id="contact" className={selectedId === 'contact' ? 'active' : ''}>Contact Me</li>
+      </ul>
+    </nav>
+  }
+}
