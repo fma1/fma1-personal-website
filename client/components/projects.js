167cd2c89608e798377add117d4d88f52fc80b76
Fred Ma <fma1@terpmail.umd.edu>
2018-06-06 16:41:34 -0400

    Added flexbox to projects and scaled images to 50% width

--- client/components/projects.js
+++ client/components/projects.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 
 const Projects = () =>
-  <div>
-    <div>
+  <div id="projects">
+    <div className="projectItem">
       <h3>DevBook</h3>
       <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256184/devbook_v1e0ma.png" />
     </div>
+    <div className="projectItem">
       <h3>LingoAssim</h3>
       <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256185/lingoassim_ucymq3.png" />
-    <div>
     </div>
+    <div className="projectItem">
       <h3>Silikon Road</h3>
       <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256185/silikonroad_deehvb.png" />
-    <div>
     </div>
   </div>
 
