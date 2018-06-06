af9661d5fa0a57b0e500bb62f22dd91956ced109
Fred Ma <fma1@terpmail.umd.edu>
2018-06-05 23:38:30 -0400

    Added projects

--- client/components/projects.js
+++ client/components/projects.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react'
 
 const Projects = () =>
   <div>
-    <h2>Projects</h2>
+    <div>
+      <h3>DevBook</h3>
+      <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256184/devbook_v1e0ma.png" />
+    </div>
+      <h3>LingoAssim</h3>
+      <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256185/lingoassim_ucymq3.png" />
+    <div>
+    </div>
+      <h3>Silikon Road</h3>
+      <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256185/silikonroad_deehvb.png" />
+    <div>
+    </div>
   </div>
 
 export default Projects 
