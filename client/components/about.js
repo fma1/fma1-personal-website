10c2e9a90f2bcdb5945e39cdaf506f919dffc5f3
Fred Ma <fma1@terpmail.umd.edu>
2018-06-19 21:24:37 -0400

    Added paragraph to About page

--- client/components/about.js
+++ client/components/about.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 const About = () =>
   <div>
     <h3>About Me</h3>
+    <p>I am someone who has been coding from a young age as my father had introduced it me. To me, coding was just like legos. I just had fun building stuff. In college, I majored in Computer Science. And through my learning experiences in college, I learned to become emphathetic towards others and wanted to help others. I then attended Fullstack Academy to get more experience, and I learned about Full Stack Web Development. With how ubiquitous software is today, I believe in the power of software and I want to make a positive impact on society using my skills.</p>
   </div>
 
 export default About
