0a5411be1bd3ae291157dd50da9b8c8a798c0e24
Fred Ma <fma1@terpmail.umd.edu>
2018-06-19 21:23:50 -0400

    Added links for projects

--- client/components/projects.js
+++ client/components/projects.js
@@ -4,15 +4,21 @@ const Projects = () =>
   <div id="projects">
     <div className="projectItem">
       <h3>DevBook</h3>
-      <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256184/devbook_v1e0ma.png" />
+      <a href="https://codesnippets-c9eee.firebaseapp.com">
+        <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256184/devbook_v1e0ma.png" />
+      </a>
     </div>
     <div className="projectItem">
       <h3>LingoAssim</h3>
-      <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256185/lingoassim_ucymq3.png" />
+      <a href="https://expo.io/@fma1/LingoAssim">
+        <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256185/lingoassim_ucymq3.png" />
+      </a>
     </div>
     <div className="projectItem">
       <h3>Silikon Road</h3>
-      <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256185/silikonroad_deehvb.png" />
+      <a href="https://silikon-road.herokuapp.com">
+        <img src="http://res.cloudinary.com/dx1uhvlo8/image/upload/v1528256185/silikonroad_deehvb.png" />
+      </a>
     </div>
   </div>
 
