c5389f3d04a54433be2cec31a2411e8266c072a0
Fred Ma <fma1@terpmail.umd.edu>
2018-06-19 21:31:39 -0400

    Added Contact Information

--- client/components/contact.js
+++ client/components/contact.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 const Contact = () =>
   <div>
     <h3>Contact Information</h3>
+    <p>Phone Number: 301-674-4666</p>
+    <p>Email Address: fredma1234@gmail.com</p>
   </div>
 
 export default Contact 
